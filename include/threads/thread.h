9cde6621e0e3adbe5dbeb60486bff9d74845a907
DGboost <147890917+DGboost@users.noreply.github.com>
2023-12-10 03:46:45 +0900

    Update thread.h
    thread.h

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -9,24 +9,24 @@
 #include "vm/vm.h"
 #endif
 
-
 /* States in a thread's life cycle. */
-enum thread_status {
-	THREAD_RUNNING,     /* Running thread. */
-	THREAD_READY,       /* Not running but ready to run. */
-	THREAD_BLOCKED,     /* Waiting for an event to trigger. */
-	THREAD_DYING        /* About to be destroyed. */
+enum thread_status
+{
+	THREAD_RUNNING, /* Running thread. */
+	THREAD_READY,	/* Not running but ready to run. */
+	THREAD_BLOCKED, /* Waiting for an event to trigger. */
+	THREAD_DYING	/* About to be destroyed. */
 };
 
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
-#define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
+#define TID_ERROR ((tid_t)-1) /* Error value for tid_t. */
 
 /* Thread priorities. */
-#define PRI_MIN 0                       /* Lowest priority. */
-#define PRI_DEFAULT 31                  /* Default priority. */
-#define PRI_MAX 63                      /* Highest priority. */
+#define PRI_MIN 0	   /* Lowest priority. */
+#define PRI_DEFAULT 31 /* Default priority. */
+#define PRI_MAX 63	   /* Highest priority. */
 
 /* A kernel thread or user process.
  *
@@ -85,34 +85,34 @@ typedef int tid_t;
  * only because they are mutually exclusive: only a thread in the
  * ready state is on the run queue, whereas only a thread in the
  * blocked state is on a semaphore wait list. */
-struct thread 
+struct thread
 {
 	/* Owned by thread.c. */
-	tid_t tid;                          /* Thread identifier. */
-	enum thread_status status;          /* Thread state. */
-	char name[16];                      /* Name (for debugging purposes). */
-	int priority;                       /* Priority. */
-
+	tid_t tid;				   /* Thread identifier. */
+	enum thread_status status; /* Thread state. */
+	char name[16];			   /* Name (for debugging purposes). */
+	int priority;			   /* Priority. */
+	
 	/* alarm clock */
-	int64_t time_to_wakeup;
+	int64_t time_to_wakeup;	   /* Time to wake up (for sleeping thread) */
 
 	/* Shared between thread.c and synch.c. */
-	struct list_elem elem;              /* List element. */
+	struct list_elem elem; /* List element. */
 
-	/* donation priority */         
-	int init_priority;  
-	struct lock *wait_on_lock;
-	struct list donations;
-	struct list_elem donations_elem; 
+	/* priority scheduling */
+	int init_priority;				/* donation 이후 우선순위를 초기화하기 위해 초기값 저장 */
+	struct lock *wait_on_lock;		/* 해당 스레드가 대기 하고 있는 lock자료구조의 주소를 저장 */
+	struct list donations;			/* multiple donation 을 고려하기 위해 사용 */
+	struct list_elem donation_elem; /* multiple donation 을 고려하기 위해 사용 */
 
-	/* mlfqs */
-	int nice; 
+	/* MLFQS */
+	int nice; /* for aging */
 	int recent_cpu;
-	struct list_elem allelem;
+	struct list_elem allelem; /* 모든 thread의 recent_cpu와 priority값 재계산하기 위함 */
 
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
-	uint64_t *pml4;                     /* Page map level 4 */
+	uint64_t *pml4; /* Page map level 4 */
 #endif
 #ifdef VM
 	/* Table for whole virtual memory owned by thread. */
@@ -120,8 +120,8 @@ struct thread
 #endif
 
 	/* Owned by thread.c. */
-	struct intr_frame tf;               /* Information for switching */
-	unsigned magic;                     /* Detects stack overflow. */
+	struct intr_frame tf; /* Information for switching */
+	unsigned magic;		  /* Detects stack overflow. */
 };
 
 /* If false (default), use round-robin scheduler.
@@ -129,50 +129,45 @@ struct thread
    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
-void thread_init (void);
-void thread_start (void);
+void thread_init(void);
+void thread_start(void);
 
-void thread_tick (void);
-void thread_print_stats (void);
+void thread_tick(void);
+void thread_print_stats(void);
 
-typedef void thread_func (void *aux);
-tid_t thread_create (const char *name, int priority, thread_func *, void *);
+typedef void thread_func(void *aux);
+tid_t thread_create(const char *name, int priority, thread_func *, void *);
 
-void thread_block (void);
-void thread_unblock (struct thread *);
+void thread_block(void);
+void thread_unblock(struct thread *);
 
-struct thread *thread_current (void);
-tid_t thread_tid (void);
-const char *thread_name (void);
+struct thread *thread_current(void);
+tid_t thread_tid(void);
+const char *thread_name(void);
 
-void thread_exit (void) NO_RETURN;
-void thread_yield (void);
+void thread_exit(void) NO_RETURN;
+void thread_yield(void);
 
-int thread_get_priority (void);
-void thread_set_priority (int);
+int thread_get_priority(void);
+void thread_set_priority(int);
 
-int thread_get_nice (void);
-void thread_set_nice (int);
-int thread_get_recent_cpu (void);
-int thread_get_load_avg (void);
+int thread_get_nice(void);
+void thread_set_nice(int);
+int thread_get_recent_cpu(void);
+int thread_get_load_avg(void);
 
-void do_iret (struct intr_frame *tf);
+void do_iret(struct intr_frame *tf);
 
-#endif /* threads/thread.h */
-
-/* alarm clock */
-void thread_sleep(int64_t ticks);
-void thread_awake(int64_t ticks);
-
-bool compare_thread_priority(const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED);
+// ----------create---------- //
 
 void thread_preemption(void);
 
-/* donation priority */
-void remove_with_lock(struct lock *lock);
-bool compare_donation_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
-void refresh_priority(void);
+/* priority scheduling */
+bool compare_thread_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
+bool compare_donation_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 void donate_priority(void);
+void remove_with_lock(struct lock *lock);
+void refresh_priority(void);
 
 /* mlfqs */
 void mlfqs_priority(struct thread *t);
@@ -181,3 +176,5 @@ void mlfqs_load_avg(void);
 void mlfqs_increment(void);
 void mlfqs_recalc_priority(void);
 void mlfqs_recalc_recent_cpu(void);
+
+#endif /* threads/thread.h */
